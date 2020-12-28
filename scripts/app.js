2b2473587acce052f4ea118d209b30f40bb27fa0
smashflashtech <smash.flash.tech@gmail.com>
2020-12-27 23:02:42 -0800

    toggles plus and minus on accordian

--- scripts/app.js
+++ scripts/app.js
@@ -78,20 +78,16 @@ const dlMode = () => {
 // PROJECTS - Accordian
 
 const acc = document.getElementsByClassName("accordion");
-
-
 for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
     this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
     let panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
+      document.getElementById(this.getAttribute('value')).innerHTML='+'
     } else {
       panel.style.display = "block";
+      document.getElementById(this.getAttribute('value')).innerHTML='-'
     }
   });
 }
