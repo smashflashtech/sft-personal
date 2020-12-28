bc2914c47151b02b8224333eaaca3ad795604844
smashflashtech <smash.flash.tech@gmail.com>
2020-12-27 22:35:50 -0800

    adds accordian

--- scripts/app.js
+++ scripts/app.js
@@ -74,3 +74,24 @@ const dlMode = () => {
     document.querySelector('body').style.backgroundColor = 'rgb(237, 237, 237)'
   }
 }
+
+// PROJECTS - Accordian
+
+const acc = document.getElementsByClassName("accordion");
+
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
